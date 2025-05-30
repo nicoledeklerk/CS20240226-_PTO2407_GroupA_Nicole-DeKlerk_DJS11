ee99ab04793551f64bfbad548be27017b3fcfc58
Nicole de Klerk <nicolenataliedeklerk@gmail.com>
2025-05-30 14:38:59 +0200

    Create Header
    Create Header with logo, slogan and navigation links

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import './Header.css';
-import { Link } from 'react-router-dom';
+// Import the Link component to enable client-side navigation //
+import { Link } from 'react-router-dom'; 
 
 export default function Header() {
   return (
